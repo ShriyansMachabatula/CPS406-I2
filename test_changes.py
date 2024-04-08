f379ce2353e9755bb0d8679645d49cc11553e0c4
Nuzina <158646721+Nuzina@users.noreply.github.com>
2024-04-08 18:21:03 -0400

    Update test_changes.py

--- test_changes.py
+++ test_changes.py
@@ -110,3 +110,12 @@ class ClubFinance:
             print(f"{self.name} has made a payment of {amount} for {practice_session}")
             self.confirm_payment()
 
+class Person():
+    def __init__(self, member_id, name, phone, balance, address):
+        self.member_id = member_id
+        self.name = name
+        self.phone = phone
+        self.balance = balance
+        self.address = address
+        self.paid = False # Default paid status is False
+        self.member_list = []
