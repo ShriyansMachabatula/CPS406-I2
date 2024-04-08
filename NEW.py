7b636a865a8ff76334e52290a494eda7887cae7b
Shriyans Machabatula <shriyans2004@gmail.com>
2024-04-08 18:39:48 -0400

    Update NEW.py

--- NEW.py
+++ NEW.py
@@ -2,16 +2,31 @@ class Main:
 
 
 
-class members: 
+class Members:
+  def create_member_list(self):
+        # List of members
+        member_list = []
+        for member_id, details in self.members.items():
+            if details['Attendance'] > 0: # Checking if they attended at least one class
+                member_info = {
+                    'MemberID': member_id,
+                    'Phone': details['Phone'],
+                    'Name': details['Name'],
+                    'Address': details['Address']
+                    'PaidSessions': details['PaidSessions'],
+                    'UnpaidSessions': details['UnpaidSessions'],
+                    'Streak': details['Streak'],
+                }
+                member_list.append(member_info)
+        return member_list
+
+
+  
+class Session:
 
 
   
 
-class session:
-
-
-  
-
-class finances:
+class Finances:
 
 
