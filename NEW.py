c7b3b6a1ffb49fa258cdd4675ff388bbc1463de9
Shriyans Machabatula <shriyans2004@gmail.com>
2024-04-08 18:48:35 -0400

    Update NEW.py

--- NEW.py
+++ NEW.py
@@ -5,8 +5,11 @@ class Main:
 
 
 
+import csv 
+
 class Members:
     def __init__(self, csv_file):
+        self.members = {}  
         try:
             with open(csv_file, mode='r') as file:
                 reader = csv.DictReader(file)
@@ -16,20 +19,22 @@ class Members:
                         'Attendance': int(row.get('Attendance', 0)),
                         'PaidSessions': int(row.get('PaidSessions', 0)),
                         'UnpaidSessions': int(row.get('UnpaidSessions', 0)),
-                        'ConsecutiveMonthsPaid': int(row.get('ConsecutiveMonthsPaid', 0))
+                        'Phone': row.get('Phone', ''),
+                        'Address': row.get('Address', ''),
+                        'Streak': int(row.get('Streak', 0)), 
                     }
         except ValueError as e:
             print(f"Error loading CSV data: {e}")
-  def create_member_list(self):
-        # List of members
+            
+    def create_member_list(self):
         member_list = []
         for member_id, details in self.members.items():
-            if details['Attendance'] > 0: # Checking if they attended at least one class
+            if details['Attendance'] > 0:  
                 member_info = {
                     'MemberID': member_id,
                     'Phone': details['Phone'],
                     'Name': details['Name'],
-                    'Address': details['Address']
+                    'Address': details['Address'],
                     'PaidSessions': details['PaidSessions'],
                     'UnpaidSessions': details['UnpaidSessions'],
                     'Streak': details['Streak'],
@@ -37,6 +42,9 @@ class Members:
                 member_list.append(member_info)
         return member_list
 
+    def Paid_sorted(self):
+        sorted_members = sorted(self.members.values(), key=lambda x: x['PaidSessions'], reverse=True)
+        return sorted_members
 
   
 class Session:
